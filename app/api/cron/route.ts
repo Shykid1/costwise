bbeea154feb4efed8a4b7d96953524acadf63b3e
Yussif Yahuza <yussifyahuza12@gmail.com>
2023-11-17 04:08:20 +0000

    fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
 } from "../../../lib/util";
 import { generateEmailBody, sendEmail } from "../../../lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 9; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
